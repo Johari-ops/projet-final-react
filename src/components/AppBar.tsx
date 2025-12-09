04e3ae310670c548b509a5904b0f12e9dca2fea8
Johari-ops <joseph.baert@student.junia.com>
2025-12-09 10:10:33 +0100

    add axios + avatar perso

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -55,6 +55,35 @@ function ResponsiveAppBar(props: any) {
         onNavigate?.(page);
     };
 
+    function stringAvatar(name: string) {
+        return {
+            sx: {
+                bgcolor: stringToColor(name),
+            },
+            children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+        };
+    }
+
+    function stringToColor(string: string) {
+        let hash = 0;
+        let i;
+
+        /* eslint-disable no-bitwise */
+        for (i = 0; i < string.length; i += 1) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        let color = '#';
+
+        for (i = 0; i < 3; i += 1) {
+            const value = (hash >> (i * 8)) & 0xff;
+            color += `00${value.toString(16)}`.slice(-2);
+        }
+        /* eslint-enable no-bitwise */
+
+        return color;
+    }
+
     return (
         <AppBar position="fixed">
             <Container maxWidth="xl">
@@ -188,7 +217,7 @@ function ResponsiveAppBar(props: any) {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                                <Avatar {...stringAvatar(nom + " " + prenom)} />
                             </IconButton>
                         </Tooltip>
                         <Menu
